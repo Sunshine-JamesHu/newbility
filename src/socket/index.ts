45be7daedcba5b907e889c62e0cb6e04c01dfc18
Sunshine-JamesHu <690531347@qq.com>
2023-09-20 21:53:54 +0800

    feat:导出Socket的相关

--- src/socket/index.ts
+++ src/socket/index.ts
@@ -1,2 +1,14 @@
+import { SOCKET_HUB_INJECT_TOKEN, ISocketHub, SocketHub, Receive, GetReceiveTopic } from './src/SocketHub';
 import { SocketModule } from './src/SocketModule';
-export { SocketModule };
+import { CreateSocketServer, GetSocketServer, AttachToServer } from './src/SocketServer';
+export {
+  SocketModule,
+  SOCKET_HUB_INJECT_TOKEN,
+  ISocketHub,
+  SocketHub,
+  Receive,
+  GetReceiveTopic,
+  CreateSocketServer,
+  GetSocketServer,
+  AttachToServer,
+};
